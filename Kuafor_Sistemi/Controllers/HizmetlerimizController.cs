c616cccbcaa545146f907aa67cce9b50f85fce19
Hüdanur Duranoğlu <hudanur.duranoglu@gmail.com>
2024-12-17 22:07:24 +0300

    düzenlemeler yapıldı

---
+++ Kuafor_Sistemi/Controllers/HizmetlerimizController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Kuafor_Sistemi.Models;
+
+namespace Kuafor_Sistemi.Controllers
+{
+    public class HizmetlerimizController : Controller
+    {
+        Context c = new Context();
+        public IActionResult Index()
+        {
+            var degerler = c.islemlers.ToList();
+            return View(degerler);
+        }
+    }
+}
