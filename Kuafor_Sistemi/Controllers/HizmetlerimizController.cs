4bb04deefdd325fd26131e43e86db4f17547d644
Hüdanur Duranoğlu <hudanur.duranoglu@gmail.com>
2024-12-22 01:09:53 +0300

    Authentication eklendi

--- Kuafor_Sistemi/Controllers/HizmetlerimizController.cs
+++ Kuafor_Sistemi/Controllers/HizmetlerimizController.cs
@@ -1,14 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Kuafor_Sistemi.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Kuafor_Sistemi.Controllers
 {
     public class HizmetlerimizController : Controller
     {
-        Context c = new Context();
+        private readonly Context _context;
+
+        // Context nesnesini yapıcı aracılığıyla enjekte ediyoruz.
+        public HizmetlerimizController(Context context)
+        {
+            _context = context;
+        }
+
+        [Authorize]
         public IActionResult Index()
         {
-            var degerler=c.Islemlers.ToList();
+            // Context kullanılarak veritabanından veriler çekiliyor.
+            var degerler = _context.Islemlers.ToList();
             return View(degerler);
         }
     }
--- Kuafor_Sistemi/Program.cs
+++ Kuafor_Sistemi/Program.cs
@@ -1,5 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using Kuafor_Sistemi.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,8 +16,19 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<Context>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/Account/Login"; // Giriþ yapmayan kullanýcýyý yönlendirme yolu
+        
+    });
+
+
 // MVC'yi ekliyoruz
 builder.Services.AddControllersWithViews();
+
+
 
 var app = builder.Build();
 
@@ -24,6 +44,7 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+app.UseAuthentication(); // Kimlik doðrulama
 
 app.MapControllerRoute(
     name: "default",
